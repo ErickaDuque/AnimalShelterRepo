ddfedeac2d151a90e22820f4524484bceef78eb5
ericka.duque <ericka.duque@pointwest.com.ph>
2017-11-21 19:51:20 +0800

    Add Employee bean

---
+++ src/com/pointwest/training/beans/Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+	
+	private int employeeId;
+	
+	public Employee(){
+		this.employeeId;
+	}
+	
+	public void setEmployeeId(int employeeId){
+		this.employeeId = employeeId;
+	}
+	
+	public int getEmployeeId(){
+		return employeeId;
+	}
+}
